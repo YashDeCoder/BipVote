dad760d2ab1946ef149990c1d63f7213e024d6cc
Yash <yashisrani35@gmail.com>
2024-04-26 00:08:54 +0200

    Added count

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ from googletrans import Translator
 #pip install googletrans==4.0.0rc1
 
 app = Flask(__name__)
-
+countNo = 0
+countYes = 0
 
 @app.route("/")
 def hello_world():
     return "<p>API running! Here's our website</p>"
+
+@app.route("/count/<YesNo>", methods=['POST'])
+def resultsCount(YesNo= None):
+    global countYes, countNo
+    if int(YesNo) == 1:
+        countYes += 1
+    else:
+        countNo += 1
+    return f'The amount of votes for yes is {countYes} and the amount of votes for no is {countNo}'
 
 
 @app.route("/speak/<lang>/<firstOption>/<secondOption>", methods=['GET'])
