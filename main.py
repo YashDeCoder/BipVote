d88ded8c6c6faa398da929e3f5293f69555f1d5a
Yash <yashisrani35@gmail.com>
2024-05-13 21:05:14 +0200

    Hash fix, comments, hosting and folders added

--- main.py
+++ main.py
@@ -6,10 +6,25 @@ import os
 
 app = Flask(__name__)
 
+## HOME PAGE
+@app.route("/")
+def hello_world():
+    countYes, countNo = current_vote()
+    return "<p>API running! Here's our website</p>" + f'The amount of votes for yes is {countYes} and the amount of votes for no is {countNo}'
+
+## AUXILLIARY METHODS
+# Getting the current amount of votes
+def current_vote():
+    countYes = read_last_number_from_file('votes/votes_yes.txt')
+    countNo = read_last_number_from_file('votes/votes_no.txt')
+    return countYes, countNo
+
+# Save the vote
 def save_number_to_file(number, filename):
     with open(filename, 'a') as file:
         file.write(str(number) + '\n')
 
+# Reading the most recent count
 def read_last_number_from_file(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
@@ -18,70 +33,120 @@ def read_last_number_from_file(filename):
         else:
             return None
 
+# Reset all text files
 def reset_files(*filenames):
     for filename in filenames:
         with open(filename, 'w') as file:
-            file.write('0\n')
+            if (filename != 'votes/votes_nums.txt'):
+                file.write('0\n')
 
-# Home page
-@app.route("/")
-def hello_world():
-    countNo = read_last_number_from_file('votesYes.txt')
-    countYes = read_last_number_from_file('votesNo.txt')
-    return "<p>API running! Here's our website</p>" + f'The amount of votes for yes is {countYes} and the amount of votes for no is {countNo}'
+# Making number a hash value
+def hash_number(number):
+    hashedNum = hash(number)
+    with open('votes/votes_nums.txt', 'r') as file:
+        for line in file:
+            if str(hashedNum) in line.strip():
+                return True
+    save_number_to_file(hashedNum, 'votes/votes_nums.txt')
+    return False
 
 # Registering a vote
-@app.route("/count/<YesNo>", methods=['GET'])
-def resultsCount(YesNo= None):
-    countYes = read_last_number_from_file('votesYes.txt')
-    countNo = read_last_number_from_file('votesNo.txt')
+def register_count(YesNo, number):
+    yes, no = current_vote()
+    if hash_number(number):
+        return False
     if int(YesNo) == 1:
-        countYes += 1
+        yes += 1
+        save_number_to_file(yes,'votes/votes_yes.txt')
     else:
-        countNo += 1
-    text = f'The amount of votes for yes is {countYes} and the amount of votes for no is {countNo}'
-    obj = gTTS(text=text, slow=False)
-    obj.save('registeredVotes.wav')
-    return send_file('registeredVotes.wav')
+        no += 1
+        save_number_to_file(no,'votes/votes_no.txt')
+    return True
 
+## HTTP REQUESTS
 # Reset the votes
 @app.route("/reset/<lang>", methods=['GET'])
-def resultsSpoken(lang="en"):
-    firstOption = read_last_number_from_file('votesYes.txt')
-    secondOption = read_last_number_from_file('votesNo.txt')
+def results_reset(lang="en"):
+    firstOption = read_last_number_from_file('votes/votes_yes.txt')
+    secondOption = read_last_number_from_file('votes/votes_no.txt')
     winner_text = "first option" if int(firstOption) > int(secondOption) else "second option"
     winner_number = firstOption if int(firstOption) > int(secondOption) else secondOption
     loser_number = secondOption if int(firstOption) > int(secondOption) else firstOption
-    reset_files('votesNo.txt', 'votesYes.txt')
     text = f'The winner of the vote is the {winner_text}, with {winner_number} votes compared to {loser_number} votes'
     translator = Translator()
     translation = translator.translate(text, src='en', dest=lang)
-    print(translation.text)
     obj = gTTS(text=translation.text, slow=False, lang=lang)
-    obj.save('winner.wav')
-    return send_file('winner.wav')
+    obj.save('audio/winner.wav')
+    reset_files('votes/votes_no.txt', 'votes/votes_yes.txt', 'votes/votes_nums.txt')
+    return send_file('audio/winner.wav')
+
+@app.route("/votes/<lang>", methods=['GET'])
+def result_voice(lang="en"):
+    countYes, countNo = current_vote()
+    text = f'The amount of votes for yes is {countYes} and the amount of votes for no is {countNo}'
+    translator = Translator()
+    translation = translator.translate(text, src='en', dest=lang)
+    obj = gTTS(text=translation.text, slow=False, lang=lang)
+    obj.save('audio/registeredVotes.wav')
+    return send_file('audio/registeredVotes.wav')
+
+## HOSTING VXML FILES 
+@app.route("/vxml_no")
+def voteno():
+    number = str(request.args.get('session.callerid'))
+    register_count(0, number)
+    return send_from_directory(app.static_folder, 'voters/vote_no.vxml')
+
+@app.route("/vxml_yes")
+def voteyes():
+    number = str(request.args.get('session.callerid'))
+    register_count(1, number)
+    return send_from_directory(app.static_folder, 'voters/vote_yes.vxml')
+
+@app.route("/vxml_organizers")
+def organizers():
+    return send_from_directory(app.static_folder, "organizers/organizers.vxml")
+
+@app.route("/organizers_en")
+def en_organizers():
+    return send_from_directory(app.static_folder, "organizers/englishMenu.vxml")
+
+@app.route("/organizers_fr")
+def fr_organizers():
+    return send_from_directory(app.static_folder, "organizers/frenchMenu.vxml")
+
+## HOSTING AUDIO FILES
+@app.route("/language_choice")
+def language_choice():
+    return send_file("audio/languageChoice.wav")
+
+@app.route("/winner")
+def winner():
+    return send_file("audio/winner.wav")
+
+@app.route("/amount_count")
+def votes():
+    send_from_directory('audio', "registeredVotes.wav")
+
+@app.route("/frMain")
+def fr_main():
+    return send_file("audio/frMain.wav")
+
+@app.route("/frFetchVotes")
+def fr_fetch_votes():
+    return send_file("audio/frFetchVotes.wav")
+
+@app.route("/frResetVotes")
+def fr_reset_votes():
+    return send_file("audio/frResetVotes.wav")
 
 
 # To get wav files for certain audio
 @app.route("/voice")
 def voteVoice():
-    enText = "Press 1 for English. Press 2 for French."
-    frText = "Appuyez sur 1 pour l'anglais. Appuyez sur 2 pour le français."
-    obj = gTTS(text=enText, slow=False, lang='en')
-    obj.save('audio/enChoice.wav')
-    obj = gTTS(text=frText, slow=False, lang='fr')
-    obj.save('audio/frChoice.wav')
-    return send_file('audio/frChoice.wav')
-
-# Hosting VXML files 
-@app.route("/voteNo")
-def voteno():
-    return send_from_directory(app.static_folder, 'vote_no.vxml')
-
-@app.route("/vxml_yes")
-def voteyes():
-    number = str(request.args.get('session.callerid'))
-    # making number a hash value
-    hashedNum = hash(number)
-    save_number_to_file(hashedNum, 'votes_nums.txt')
-    return send_from_directory(app.static_folder, 'vote_yes.vxml')
+    text = "The vote is being reset..."
+    translator = Translator()
+    translation = translator.translate(text, src='en', dest="fr")
+    obj = gTTS(text=translation.text, slow=False, lang="fr")
+    obj.save('audio/frResetVotes.wav')
+    return send_file('audio/frResetVotes.wav')
